42cea07a3400e44c822596df6cd74f6ddd0663ef
Paco Contreras <pacocontrerasdct@gmail.com>
2015-09-03 07:30:05 +0100

    Mortgage and BMI calc working but not accurated

--- app.rb
+++ app.rb
@@ -13,12 +13,32 @@ get '/basic' do
   erb :basic_calc
 end
 
+get '/mortgage' do
+  @title = 'Mortgage Calculator: '
+  erb :mortgage_calc
+end
+
+get '/bmi' do
+  @title = 'BMI Calculator: '
+  erb :bmi_calc
+end
+
+get '/trip' do
+  @title = 'Trip Calculator: '
+  erb :trip_calc
+end
+
+get '/cilinder' do
+  @title = 'Cilinder\'s Volume Calculator: '
+  erb :cilinder_calc
+end
+
 post '/basic' do
   # puts params
   num1 = params[:num_1].to_f
   num2 = params[:num_2].to_f
   operation = params[:operation].to_s
-  aritop = Arithmetics.new
+  aritop = CalculatorMachine.new
   @title = 'Basic Calculator: Add, rest, multiply and divide two numbers'
   puts num1
   
@@ -34,7 +54,48 @@ post '/basic' do
     result = 0
   end
 
-  @result = result.to_f.round(4)
+  @result = "Result: #{result.to_f.round(4)}"
   erb :basic_calc
+
+end
+
+post '/mortgage' do
+  # puts params
+  num1 = params[:num_1].to_f
+  num2 = params[:num_2].to_f
+  num3 = params[:num_3].to_f
+  puts params
+  aritop = CalculatorMachine.new
+  @title = 'Mortgage Calculator: '
+  puts num3
+  
+  result = aritop.mortgage num1, num2, num3
+
+  @result = "Result:Your Monthly Payment would be #{result.to_f.round(2)} pounds."
+  erb :mortgage_calc
+
+end
+
+post '/bmi' do
+  # puts params
+  num1 = params[:num_1].to_f
+  num2 = params[:num_2].to_f
+  system = params[:system].to_s
+  puts params
+  aritop = CalculatorMachine.new
+  @title = 'BMI Calculator: '
+
+  if system == "metric"
+    result = aritop.bmi_metric num1, num2
+    result = "#{result.to_f.round(2)} kilos by square metre."
+  elsif system == "imperial"
+    result = aritop.bmi_imperial num1, num2
+    result = "#{result.to_f.round(2)} libras by square inch."
+  else
+    result = 0
+  end
+
+  @result = "Result: Your Body Mass Index is ".concat result
+  erb :bmi_calc
 
 end
--- models/basic_aritmethic.rb
+++ models/basic_aritmethic.rb
@@ -1,10 +1,12 @@
-class Arithmetics
+class CalculatorMachine
 
-  attr_accessor :num1, :num2, :result
+  attr_accessor :num1, :num2, :num3, :num4, :result
   
   def initialize  
     @num1 = num1
     @num2 = num2
+    @num3 = num3
+    @num4 = num4
     @result = result
   end
   
@@ -23,5 +25,33 @@ class Arithmetics
   def div num1, num2
     result = num1 / num2
   end
+
+  def mortgage num1, num2, num3
+    # num1 is Loan, num2 is Apr, num3 is Term
+    num2 = (num2 * 0.10) / 12
+    brackets = (1 + num2)** num3
+    result = num1 * ((num2 * brackets) / brackets - 1)
+  end
+
+  def bmi_metric num1, num2
+    result = num1 / (num2** 2)
+  end
+
+  def bmi_imperial num1, num2
+    result = num1 / (num2** 2) * 703
+  end
+
+  def trip num1, num2, num3, num4
+    # num_1= Distance, num_2=MPG, num_3=Cost, num_4=Speed
+ 
+    # var newFuelEffic = parseFloat(tripNum4) - 60;
+    # newMPG = parseFloat(tripNum2) - (newFuelEffic * 2);
+    # time = (parseFloat(tripNum1) / parseFloat(tripNum4)) / 60;
+    # money = parseFloat(newMPG) * parseFloat(tripNum1) * parseFloat(tripNum3);
+
+    result = 1# 0 
+  end
+
+
 
 end
